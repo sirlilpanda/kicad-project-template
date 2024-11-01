8750e92c1a7cd1c2290a1abf08c59b34803b4e64
sirlilpanda <sirlilpanda@gmail.com>
2024-11-01 17:40:27 +1300

    dir for the python scripts to process the reports to come out of the erc / drc

---
+++ .github/report_processing/process_json_reports.py
@@ -0,0 +1,33 @@
+# usage: python process_rpt_files.py report.rpt template.mustache outfile.md
+import chevron 
+import sys
+import datetime
+import json
+import process_erc_json
+import process_drc_json
+from pprint import pprint
+
+
+def load_report(filename : str) -> dict:
+    out_dict : dict = {}
+    with open(filename, "r") as js:
+        if ("erc" in filename.lower()):
+            out_dict = process_erc_json.process_report(js.read())
+        if ("drc" in filename.lower()):
+            out_dict = process_drc_json.process_report(js.read())
+
+    out_dict.setdefault("time", str(datetime.datetime.now().time()))
+    out_dict.setdefault("date", str(datetime.datetime.now().date()))
+
+    return out_dict
+
+def main():
+    report_hash = load_report(sys.argv[1])
+    # pprint(report_hash)
+    with open(sys.argv[2], "r") as txt:
+        out = chevron.render(txt.read(), report_hash)
+        with open(sys.argv[3], "w") as md:
+            md.write(out)
+
+if __name__ == "__main__":
+    main()
