e1c0972ba3a200e02ad3d41cff202c0ffbe759c6
sirlilpanda <sirlilpanda@gmail.com>
2024-11-12 13:29:01 +1300

    changed output slightly so i dont have to do more scripting in bash

--- .github/get_pcb_names.py
+++ .github/get_pcb_names.py
@@ -9,7 +9,7 @@ def main():
         project_settings = yaml.safe_load(txt.read())
         project_name = project_settings["project_name"]
         subnames = project_settings["sub_pcb_names"]
-        print([project_name] + subnames)
+        print(f"matrix={[project_name] + subnames}")
 
 if __name__ == "__main__":
     main()
