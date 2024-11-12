240cf7793e6e9ae09d280bea7f9e07af514c9351
sirlilpanda <sirlilpanda@gmail.com>
2024-11-12 13:21:51 +1300

    script for getting all the kicad projects name out of the settings

---
+++ .github/get_pcb_names.py
@@ -0,0 +1,15 @@
+import sys
+import yaml
+from pathlib import Path
+
+
+def main():
+    # "../project_settings.yaml"
+    with open(Path(sys.argv[1])) as txt:
+        project_settings = yaml.safe_load(txt.read())
+        project_name = project_settings["project_name"]
+        subnames = project_settings["sub_pcb_names"]
+        print([project_name] + subnames)
+
+if __name__ == "__main__":
+    main()
