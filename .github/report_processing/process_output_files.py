1f7c4ebc161c7f737d877588242f102c1d7bd588
sirlilpanda <sirlilpanda@gmail.com>
2024-11-17 22:39:43 +1300

    check to see if hash gets constructed correctly

--- .github/report_processing/process_output_files.py
+++ .github/report_processing/process_output_files.py
@@ -7,23 +7,67 @@ import glob
 from pprint import pprint
 
 
+# erc
+# {
+#   "project_name" : "string",
+#   "passing_erc" : "bool",
+#   "erc_summary_link" : "link",  
+# }
+
+# drc
+# {
+#   "project_name" : "string",
+#   "passing_drc" : "bool",
+#   "drc_summary_link" : "link",  
+# }
+
+# project
+# {
+#     "project_name" : "string",
+#     "project_link" : "link",
+#     "schematic_link" : "link",
+#     "gerber_link" : "link",
+#     "bom_report_link" : "link",
+#     "bom_csv_link" : "link"
+# }
+
+EXTRAS_FILENAME = "readme_extras.json"
 
 def load_json_file(filename : str) -> dict:
     with open(filename, "r") as js:
         return json.loads(js.read())
 
 def create_hash(filenames : list[str]) -> dict:
-    reports_dicts = []
-    for report_name in filenames:
+    report_outs = filenames
+    report_outs.remove("readme_extras.json")
+
+    extras = load_json_file("readme_extras.json")
+
+    reports_dicts : list[dict] = []
+    for report_name in report_outs:
         reports_dicts.append(load_json_file(report_name))
 
-    pprint(reports_dicts)
+    readme_hash = {
+        **extras,
+        "projects" : [],
+        "did_error" : False,
+        "multiple_projects" : False
+    }
+    for report in reports_dicts:
+        for project in readme_hash["projects"]:
+            if project["project_name"] == report["project_name"]:
+                for key in report.keys():
+                    project.setdefault(key, report[key])
+        else:
+            project.append(report)
+
+    pprint(readme_hash)    
 
 
 def main():
     print(sys.argv)
 
-    readme_template, args = sys.argv[1:]
+    readme_template, *args = sys.argv[1:]
 
 
 
